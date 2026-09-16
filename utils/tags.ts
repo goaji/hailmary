79a7288c1f86f27c4139fb32a5d8fed297c4a19a
LS <lucistana@gmail.com>
2026-09-16 16:57:01 +0200

    Added a new article Changed folder structure for images related to articles Moved tags and categories types to shared

--- utils/tags.ts
+++
@@ -1,4 +0,0 @@
-// Controlled vocabulary for article `tags`. 
-export const TAG_IDS = ["playoffs", "super-bowl", "intrebari", "bulletpoints"] as const;
-
-export type Tag = (typeof TAG_IDS)[number];
